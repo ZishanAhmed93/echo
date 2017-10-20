72e81e8570197f2a1ba12b72977798d394613a94
Johnson Luu <John.Luu44@gmail.com>
2017-10-19 21:54:43 -0400

    Added basic view all echos

--- client/src/containers/ViewAllEchos.js
+++ client/src/containers/ViewAllEchos.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, {Component} from 'react';
 
-const ViewAllEchos = () => (
-  <div>
-    <h2>ViewPosts</h2>
+
+class ViewAllEchos extends Component {
+
+
+constructor() {
+    super();
+    this.state = {echos:[]}
+    }
+
+
+componentDidMount(){
+
+fetch('/echos')
+.then((response) => response.json())
+	.then((echos) => this.setState({echos})
+				
+		//const echo = jsonBody.map((c) => <Echo data ={c} />);
+		
+
+);
+
+}
+
+
+render()
+{
+
+	
+return(
+  <div className = "ViewAllEchos">
+  	LIST OF POSTS
+    {this.state.echos.map(echo =>
+    	<div key={echo.id}>
+    	{echo.id} : {echo.subject} </div>
+    	)}
   </div>
-
 )
+}
+}
 
 export default ViewAllEchos

0aa9945176deda24afaa9e1a551bfdd9c26c01d9
William Ventura <wrventura567@gmail.com>
2017-10-20 08:15:03 -0400

    initial commit

--- client/src/NewPost.js
+++ client/src/NewPost.js
@@ -1,1 +1,13 @@
-//This is test.
+import React from 'react';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Header</h1>
+				<h2>Content</h2>
+				<p>This is the content!!!</p>
+			</div>
+		);
+	}
+}

795e1f70693c119d87121700560660317795127b
Liang Chen <liang.r.chen@gmail.com>
2017-10-27 16:31:57 -0400

    Move design folder to client/src, add Logo to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React, { Component }from 'react'
 import { Link } from 'react-router-dom'
+import Logo from '../design/32.svg';
 import './navbar.css';
 
 class Navbar extends Component {
   render() {
     return(
       <nav className="navbar navbar-expand-lg navbar-light" id="navbar">
-        <a className="navbar-brand" href="#">Echo Logo</a>
+        <a className="navbar-brand" href="/"><img src={Logo} className="mr-1" alt="Echo" /></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>

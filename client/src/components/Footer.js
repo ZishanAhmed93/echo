f1353ea46113b7341b2c7b9bc52f650a974d5c56
Liang Chen <liang.r.chen@gmail.com>
2017-10-27 15:56:52 -0400

    Add font awesome, home page, footer, and style for navbar

---
+++ client/src/components/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import './Footer.css';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid text-center">
+        <div className="row navbar-light" id="footer">
+          <div className="col">
+            <nav className="nav justify-content-center mt-4 footer-link">
+              <Link to="/" className="nav-link">Home</Link>
+              <Link to="/about" className="nav-link">About</Link>
+              <Link to="/faq" className="nav-link">FAQ</Link>
+              <Link to="/contact" className="nav-link">Contact</Link>
+            </nav>
+            <nav className="nav justify-content-center footer-link">
+              <a className="nav-link pr-2" href="http://www.facebook.com"><i className="fa fa-facebook-official" aria-hidden="true"/></a>
+              <a className="nav-link pr-2" href="https://www.pinterest.com"><i className="fa fa-instagram" aria-hidden="true"/></a>
+              <a className="nav-link pr-2" href="https://www.twitter.com"><i className="fa fa-twitter" aria-hidden="true"/></a>
+              <a className="nav-link" href="https://www.tumblr.com"><i className="fa fa-tumblr-square" aria-hidden="true"/></a>
+            </nav>
+            <p className="mt-2 mb-4">Echo &copy; 2017</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,30 +1,12 @@
 import React, {Component} from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
-//components
 import Navbar from '../components/Navbar'
-
-//containers
+import Home from '../components/Home';
+import Footer from '../components/Footer';
 import NewEcho from './NewEcho'
 import ViewAllEchos from './ViewAllEchos'
 import ViewEcho from './ViewEcho'
-//import ViewComments from './ViewComments'
-
-
-
-// get     /echos                -ViewAllEchos.js
-// post    /echos                -NewEcho.js
-
-// get     /echos/:id            -ViewEcho.js
-// post    /echos/:id/comments   -ViewEcho.js
-// put     /echos/comments/:id   -ViewEcho.js
-// delete  /echos/comments/:id   -ViewEcho.js
-
 
 class App extends Component {
   constructor(){
@@ -43,20 +25,12 @@ class App extends Component {
             <Route path="/newecho" component={NewEcho}/>
             <Route path="/echos" component={ViewAllEchos}/>
             <Route path="/echo/:id" component={ViewEcho}/>
-            
           </Switch>
+          <Footer />
         </div>
       </Router>
     );
   }
 }
-
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
-
 
 export default App;

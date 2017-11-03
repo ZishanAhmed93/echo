8e3162418834aa150fbe0ab31f73de2653b664b4
Liang Chen <liang.r.chen@gmail.com>
2017-11-03 15:37:22 -0400

    Fix minor bug

--- client/src/components/DashboardPage.js
+++ client/src/components/DashboardPage.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './DashboardPage.css';
-import ViewAllEchos from '../containers/ViewAllEchos'
+import Echos from '../containers/Echos'
 
 class DashboardPage extends Component {
   render() {
     return(
       <div className="container text-center" id="dashboard-content">
-        {ViewAllEchos}
+        {Echos}
         <p>placeholder for new echo button</p>
           
       </div>

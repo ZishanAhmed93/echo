03e5a04c69e42115b677dcc7dd0ebe42892f714f
William Ventura <wrventura567@gmail.com>
2017-12-01 15:55:39 -0500

    commit unstaged changes

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -21,7 +21,7 @@ import ViewEcho from './ViewEcho';
 //import HomePage from './HomePage';
 //import Registration from './Registration'
 //things not implemented yet
-//import ProfilePage from './ProfilePage';
+import ProfilePage from './ProfilePage';
 //import TopEchosPage from './TopEchosPage';
 
 
@@ -95,7 +95,7 @@ class App extends Component {
             <PrivateRoute isAuthed={this.state.isAuthed} path="/echos" component={Echos} />
             <Route path="/newecho" component={NewEcho}/>
             <Route path="/echo/:id" component={ViewEcho}/>
-
+						<Route path="/profilepage" component={ProfilePage}/>
           </Switch>
           <Footer />
         </div>

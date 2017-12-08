157ed63e64530d167efb9642cc7bb15695cd3761
William Ventura <wrventura567@gmail.com>
2017-12-08 10:29:54 -0500

    adding changes to profile.

--- client/src/containers/ProfilePage.js
+++ client/src/containers/ProfilePage.js
@@ -8,25 +8,25 @@ import Echos from '../containers/Echos';
 //Shell for the Profile Page
 
 class ProfilePage extends Component {
-	render() {
-		return(
-			<div className="container text-center" id="profile-content">
-					<div className="profile-area">
-					<h1>John Doe</h1>
-					<Panel header="Profile">
-						<img src={testImage} alt="profile" />
-						<div>
-						<ControlLabel><Glyphicon glyph="user" /> Nickname</ControlLabel>
-						</div>
-						<div>
-						<ControlLabel><Glyphicon glyph="user" /> Email</ControlLabel>
-						</div>
-						<pre></pre>
-					</Panel>
-					</div>
-			</div>
-		);
-	}
+  render() {
+    return(
+      <div className="container text-center" id="profile-content">
+          <div className="profile-area">
+          <h1>John Doe</h1>
+          <Panel header="Profile">
+            <img src={testImage} alt="profile" />
+            <div>
+            <ControlLabel><Glyphicon glyph="user" /> Nickname</ControlLabel>
+            </div>
+            <div>
+            <ControlLabel><Glyphicon glyph="user" /> Email</ControlLabel>
+            </div>
+            <pre></pre>
+          </Panel>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default ProfilePage;

613a60bade6c42b9d9f904cc38da52cd0573db62
Liang Chen <liang.r.chen@gmail.com>
2017-12-08 20:46:43 -0500

    Fix Recho bug

--- server/middlewares/rechosAlgorithm.js
+++ server/middlewares/rechosAlgorithm.js
@@ -5,53 +5,60 @@ const models = require('../models');
 const passport = require('../middlewares/authentication');
 
 function RechosAlgorithm(echoId, senderId, res) {
-  models.Users.findAll()
-    .then(users => {
-      // retrieve only user id and filter it so it doesn't container the sender's id; prevent from
-      // sending echos to oneself.
-      let receiverIds = users.map(user => user.dataValues.id).filter(id => id !== senderId);
-      
-      // randomize receivers for now
-      receiverIds = ShuffleArray(receiverIds);
+  models.Echos.findById(parseInt(echoId))
+    .then(echo => {
+      let echoOwnerId = echo.dataValues.UserId;
 
-      // return only the first 6 user id
-      const limit = 6;
-      if(receiverIds.length > limit) {
-        receiverIds = receiverIds.slice(0, limit);
-      }
-      
-      return receiverIds;
-    })
-    .then(receiverIds => {
-      let rechos = [];
+      models.Users.findAll()
+        .then(users => {
+          // retrieve only user id and filter it so it doesn't container the sender's id; prevent from
+          // sending echos to oneself.
+          let receiverIds = users.map(user => user.dataValues.id).filter(id => {
+            return id !== senderId && id !== echoOwnerId;
+          });
+          
+          // randomize receivers for now
+          receiverIds = ShuffleArray(receiverIds);
 
-      receiverIds.forEach(receiverId => {
-        models.Rechos.create(
-          {
-            EchoId: echoId,
-            SenderId: senderId,
-            ReceiverId: receiverId,
-          },
-          {
-            include: [ { model: models.Echos }, { model: models.Users, as: 'SentEchos' }, { model: models.Users, as: 'ReceivedEchos' } ]
+          // return only the first 6 user id
+          const limit = 6;
+          if(receiverIds.length > limit) {
+            receiverIds = receiverIds.slice(0, limit);
           }
-        )
-        .then(recho => {
-          rechos.push(recho);
+          
+          return receiverIds;
+        })
+        .then(receiverIds => {
+          let rechos = [];
+
+          receiverIds.forEach(receiverId => {
+            models.Rechos.create(
+              {
+                EchoId: echoId,
+                SenderId: senderId,
+                ReceiverId: receiverId,
+              },
+              {
+                include: [ { model: models.Echos }, { model: models.Users, as: 'SentEchos' }, { model: models.Users, as: 'ReceivedEchos' } ]
+              }
+            )
+            .then(recho => {
+              rechos.push(recho);
+            })
+            .catch(err => {
+              // do something
+            });
+          });
+
+          return rechos;
+        })
+        .then(rechos => {
+          res.sendStatus(200);
         })
         .catch(err => {
-          // do something
+          res.sendStatus(400);
         });
-      });
-
-      return rechos;
     })
-    .then(rechos => {
-      res.sendStatus(200);
-    })
-    .catch(err => {
-      res.sendStatus(400);
-    });
 }
 
 function ShuffleArray(a) {

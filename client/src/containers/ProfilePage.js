f88712bd853b76b4453fc5a4d3498d8ec5a4f18d
William Ventura <wrventura567@gmail.com>
2017-12-10 23:56:27 -0500

    adding back lost changes to profile

--- client/src/containers/ProfilePage.js
+++ client/src/containers/ProfilePage.js
@@ -8,12 +8,33 @@ import Echos from '../containers/Echos';
 //Shell for the Profile Page
 
 class ProfilePage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {},
+    }
+  }
+
+  componentDidMount() {
+  fetch('/user',{
+      method: "get",
+      headers: {
+        'Accept' : 'application.json',
+        'Content-type' : 'application.json',
+      },
+      credentials: 'same-origin',
+    })
+    .then((response) => response.json())
+      .then((user) => this.setState({user})
+    );
+  }
+
   render() {
     return(
       <div className="container text-center" id="profile-content">
           <div className="profile-area">
-          <h1>John Doe</h1>
-          <Panel header="Profile">
+          <h3>{this.state.username}</h3>
+          <Panel >
             <img src={testImage} alt="profile" />
             <div>
             <ControlLabel><Glyphicon glyph="user" /> Nickname</ControlLabel>
@@ -21,8 +42,8 @@ class ProfilePage extends Component {
             <div>
             <ControlLabel><Glyphicon glyph="user" /> Email</ControlLabel>
             </div>
-            <pre></pre>
           </Panel>
+					<Echos/>
           </div>
       </div>
     );

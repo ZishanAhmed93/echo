c0a4fc837e80429fd6a72ab5e0c5c4123816fff3
William Ventura <wrventura567@gmail.com>
2017-12-13 00:12:26 -0500

    Finishing up the echo styling and integrating that into the Profile page. I did not get the functionality to work, but it looks presentable.

--- client/src/containers/Echos.js
+++ client/src/containers/Echos.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import moment from "moment";
 
 class Echos extends Component {
   constructor() {
@@ -25,16 +26,29 @@ class Echos extends Component {
 
   render() {
     return(
-      <div>
-      	ECHOS
+      <div className = "container">
+      	MY ECHOS
         {this.state.echos.map(echo =>
-        	<div key={echo.id}>
-            <li>{echo.subject}</li>
-        	 </div>
+          <div key={echo.id}>
+            <div className="tile">
+            <div className ="tileHeader black54"><span className ="pull-right"> {moment(echo.createdAt).format('MMM. d')} </span></div>
+            <div className = "tileBody">{echo.subject}</div>
+            <span> <a className="tileFooter" href="#">Echo</a>
+            <span className = "pull-right">
+            <li className="tileFooter">
+            <Link to={`/echo/${echo.id}`}> View All Comments </Link> 
+            </li>
+             </span>
+            </span>
+            </div>
+           </div>
         	)}
       </div>
     )
   }
 }
 
+        	//<div key={echo.id}>
+           // <li>{echo.subject}</li>
+        	// </div>
 export default Echos
